3ba1135d134c6deb188b42faf821107710bbddb9
LaboneeAktar <labonipub@gmail.com>
2025-08-15 09:22:46 +0600

    Remove

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
@@ -83,11 +82,8 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
   }
 }
 run().catch(console.dir);
